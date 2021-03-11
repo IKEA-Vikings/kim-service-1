18055a4e52169ec1f26e5ac15f25e84de3cbb891
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-03-10 20:54:15 -0700

    removed references to react router; moving forward with development in branches

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route} from 'react-router';
-import {createBrowserHistory} from 'history';
 import About from './components/about.jsx';
-const newHistory = createBrowserHistory();
 
-ReactDOM.render(<Router history={newHistory}>
-  <Route path='/:productId' component={About} />
-</Router>, document.getElementById('about'));
+ReactDOM.render(<About />, document.getElementById('about'));
