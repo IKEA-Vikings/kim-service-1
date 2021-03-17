2a0f6673b9466c95e9a3430e51a2476eadf9c2ae
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-03-17 11:01:10 -0500

    allowed for access across origins so that proxy could render components / handle requests

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const port = 3003;
 
 const database = require('../database/product');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../', 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
