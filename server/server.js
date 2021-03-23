907d3ad17d339ee69c65ef8045bd06b704fe514c
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-03-22 21:32:28 -0500

    added cors to server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3003;
 
 const database = require('../database/product');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../', 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
