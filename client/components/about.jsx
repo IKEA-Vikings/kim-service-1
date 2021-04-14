5b8c82d4ac688d905a9e245769c8f3785514d38d
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-04-14 10:19:47 -0500

    added additional routes for Phucci's image service and added containers to make styling easier

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import MoreColors from './moreColors.jsx';
 import MoreSizes from './moreSizes.jsx';
+import MoreColorsSidebar from './moreColorsSidebar.jsx';
+import MoreSizesSidebar from './moreSizesSidebar.jsx';
 import styles from '../style.module.css';
+import AWS from './urls.js';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 class About extends React.Component {
   constructor(props) {
@@ -12,11 +21,15 @@ class About extends React.Component {
       category: '',
       color: '',
       price: 0,
-      size: 'size',
+      size: '[size]',
       linkedColors: [],
       linkedSizes: [],
       productAvaliable: true,
-      dataQueried: false
+      dataQueried: false,
+      view: 'normal',
+      images: [],
+      reviewNum: 0,
+      reviewAverage: 0
     };
     this.componentDidMount = this.componentDidMount.bind(this);
   }
@@ -30,14 +43,51 @@ class About extends React.Component {
       // this is for if the url is simply localhost:3003 with no id specified
       return;
     }
+    // fetch produce size information
     fetch(`/api/product/${queriedId}`)
       .then((response) => {
         return response.json();
       })
       .then((jsonResponse) => {
-        // console.log(jsonResponse);
         this.setState(jsonResponse);
-      });
+        return jsonResponse.linkedColors;
+      })
+      // fetch images if there are moreColors
+      .then((colorArray) => {
+        let moreColorsImageUrls = [];
+        let imageUrls = [];
+        for (let i = 0; i < colorArray.length; i++) {
+          imageUrls.push(`${AWS.imageAPI}${colorArray[i]}`);
+        }
+        let fetchUrlData = function(url) {
+          return axios.get(url)
+            .then(response => {
+              return {
+                success: true,
+                data: response.data[0]
+              };
+            })
+            .catch(error => {
+              return {success: false};
+            });
+        };
+
+        return Promise.all(imageUrls.map(fetchUrlData));
+      })
+      .then((moreImageArrayPromises) => {
+        let imageUrls = [];
+        for (let i = 0; i < moreImageArrayPromises.length; i++) {
+          imageUrls.push(moreImageArrayPromises[i].data);
+        }
+        let stateObject = {
+          images: imageUrls
+        };
+        this.setState(stateObject);
+      })
+      .catch(err => console.error(err));
+
+
+
     // fetching from Virgina's service for sizes
     // fetch(`http://localhost:3002/api/sizes/${queriedId}`)
     //   .then((response) => {
@@ -48,8 +98,15 @@ class About extends React.Component {
     //   });
 
     // fetch from Josh's service for star rating
-
-    // fetch from Phucci's service for images
+    // axios.get(`${AWS.reviewAPI}${queriedId}`)
+    //   .then((response) => {
+    //     console.log(response);
+    //     return response.json();
+    //   })
+    //   .then((jsonResponse) => {
+    //     console.log(jsonResponse);
+    //     // this.setState(jsonResponse);
+    //   });
   }
 
   render() {
@@ -58,15 +115,25 @@ class About extends React.Component {
     let moreColors;
     let moreSizes;
     if (this.state.productAvaliable) {
-      availability = <div className={styles.aboutAvailability}>Available for delivery in select locations</div>;
+      availability = (
+        <div className={styles.aboutAvailability}>
+          <FontAwesomeIcon icon={faTruck} className={styles.bottomIcons}/>
+          <div className={styles.aboutAvailabilityText}>Available for delivery in select locations</div>
+        </div>
+      );
     } else {
-      availability = <div className={styles.aboutAvailability}>Not available for delivery</div>;
+      availability = (
+        <div className={styles.aboutAvailability}>
+          <FontAwesomeIcon icon={faTruck} className={styles.bottomIcons}/>
+          <div className={styles.aboutAvailabilityText}>Not available for delivery</div>
+        </div>
+      );
     }
 
     if (this.state.moreOptions) {
       if (this.state.linkedColors.length !== 0) {
         // eventually these will not be ids but rather images, will need to query Phucci's service
-        moreColors = <MoreColors color={this.state.color} colorOptions={this.state.linkedColors}/>;
+        moreColors = <MoreColors color={this.state.color} colorOptions={this.state.linkedColors} images={this.state.images}/>;
       } else {
         moreColors = <div></div>;
       }
@@ -91,13 +158,20 @@ class About extends React.Component {
         <div className={styles.aboutComponent}>
           <div className={styles.aboutBrand}>{this.state.brand}</div>
           <div className={styles.aboutPrice}>${this.state.price}</div>
+          {/* <div className={styles.floatClear}></div> */}
           <div className={styles.aboutDescription}>{this.state.category}, {this.state.color}, <span className={styles.aboutSize}>{this.state.size}</span></div>
           <div className={styles.aboutReviews}>{/** query for reviews */}</div>
+          <div className={styles.extraLineHeight}> </div>
           {moreOptions}
-          <button className={styles.aboutAddToBagButton}>Add to bag</button>
-          <button className={styles.aboutHeartButton}>♥</button>
+          <div className={styles.userButtonsContainer}>
+            <button className={styles.aboutAddToBagButton}>Add to bag</button>
+            <FontAwesomeIcon icon={faHeart} className={styles.aboutHeartButton} />
+          </div>
           {availability}
-          <div className={styles.aboutCOVID}>We're sorry, due to COVID-19 delivery times are running longer than usual. We are actively working to improve these issues.</div>
+          <div className={styles.aboutCOVID}>
+            <FontAwesomeIcon icon={faInfoCircle} className={styles.bottomIcons}/>
+            <div className={styles.aboutCOVIDText}>We're sorry, due to COVID-19 delivery times are running longer than usual. We are actively working to improve these issues.</div>
+          </div>
 
         </div>
       );
