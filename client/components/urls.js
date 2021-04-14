f03057d786ff930460b0d84e1e936699650991a6
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-04-14 10:19:04 -0500

    added easily updatable location for the urls of team members AWS services

---
+++ client/components/urls.js
@@ -0,0 +1,5 @@
+module.exports = {
+  imageAPI: 'http://54.67.28.46:3004/images/sizeService/',
+  sizeAPI: 'http://18.221.34.3:3002/api/sizes/',
+  reviewAPI: 'http://ec2-100-25-191-161.compute-1.amazonaws.com/api/reviews'
+};
