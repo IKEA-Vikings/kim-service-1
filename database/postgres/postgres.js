15926f92cee06801923259c9bb535fc228c3d69a
Calvin <calvinminhla@gmail.com>
2021-05-08 22:16:23 -0700

    Can bulk add via Postgres up to 1M records. Need to break insert into chunks or use COPY for 10M.

--- database/postgres/postgres.js
+++ database/postgres/postgres.js
@@ -1,6 +1,6 @@
-const {Client} = require('pg');
+const {Client, Pool} = require('pg');
 
-const client = new Client({
+const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'sdc',
@@ -10,35 +10,35 @@ const client = new Client({
 const schema = `
   DROP TABLE products;
   CREATE TABLE IF NOT EXISTS products (
-  id SERIAL NOT NULL PRIMARY KEY,
+  id INTEGER NOT NULL PRIMARY KEY,
   brand VARCHAR(15) NOT NULL,
   category VARCHAR(20) NOT NULL,
   color VARCHAR(15) NOT NULL,
   price NUMERIC(4,1) NOT NULL,
-  linkedColors SMALLINT[],
-  linkedSizes SMALLINT[],
+  linkedColors INTEGER ARRAY[5],
+  linkedSizes INTEGER ARRAY[4],
   newProduct BOOLEAN NOT NULL,
   productAvailable BOOLEAN NOT NULL
 );
 `;
 
-const createTable = async () => {
-  await client.connect();
-  client.query(schema)
-    .then(res => {
-      console.log('Table successfully created');
-    })
-    .catch(err => {
-      console.log('Error creating table', err);
-    })
-    .finally(() => {
-      client.end();
-    });
-};
+// const createTable = async () => {
+//   await pool.connect();
+//   pool.query(schema)
+//     .then(res => {
+//       console.log('Table successfully created');
+//     })
+//     .catch(err => {
+//       console.log('Error creating table', err);
+//     })
+//     .finally(() => {
+//       pool.end();
+//     });
+// };
 
 
 
 module.exports = {
-  client,
-  createTable
+  pool,
+  schema
 };
--- database/postgres/seedPostgres.js
+++ database/postgres/seedPostgres.js
@@ -1,53 +1,64 @@
-const {client, createTable} = require('./postgres.js');
+const now = require('performance-now');
+const {pool, schema} = require('./postgres.js');
 const dummyData = require('../dummy-data.js');
 
-
-
 const seedPostgres = async () => {
-  await client.connect();
-  const data = dummyData.generateRandomEntries();
-  console.log(data);
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].linkedColors.length === 0) {
-      data[i].linkedColors = 'ARRAY[]::INTEGER[]';
-    }
-    if (data[i].linkedSizes.length === 0) {
-      data[i].linkedSizes = 'ARRAY[]::INTEGER[]';
-    }
-    let query = `
-    INSERT INTO products (
-      id,
-      brand,
-      category,
-      color,
-      price,
-      linkedColors,
-      linkedSizes,
-      newProduct,
-      productAvailable
-    )
-    VALUES (
-      ${data[i]._id},
-      '${data[i].brand}',
-      '${data[i].category}',
-      '${data[i].color}',
-      ${data[i].price},
-      ${data[i].linkedSizes},
-      ${data[i].linkedColors},
-      ${data[i].newProduct},
-      ${data[i].productAvailable}
-    )
-    `;
-    console.log(query);
-    client.query(query)
-      .then(() => {
-        console.log('Successfully seeded Postgres database');
-      })
+  const start = now();
+
+  const createTable = async () => {
+    await pool.connect();
+    pool.query(schema)
       .catch(err => {
-        console.log('Error inserting into Postgres', err);
+        console.log('Error creating table', err);
       });
-  }
-  await client.end();
+  };
+  await createTable();
+
+  const data = dummyData.generateRandomEntries();
+
+  await pool.connect()
+    .then(async () => {
+      for (let i = 0; i < data.length; i++) {
+        let query = `
+          INSERT INTO products (
+            id,
+            brand,
+            category,
+            color,
+            price,
+            linkedColors,
+            linkedSizes,
+            newProduct,
+            productAvailable
+          )
+          VALUES (
+            ${data[i]._id},
+            '${data[i].brand}',
+            '${data[i].category}',
+            '${data[i].color}',
+            ${data[i].price},
+            '{${data[i].linkedSizes}}'::INTEGER[],
+            '{${data[i].linkedColors}}'::INTEGER[],
+            ${data[i].newProduct},
+            ${data[i].productAvailable}
+          )
+        `;
+
+        await pool.query(query)
+          .catch(err => {
+            console.log('Error inserting into Postgres', err);
+          });
+      }
+    })
+    .catch(err => {
+      console.log('Error connecting to Postgres database', err);
+    })
+    .finally(() => {
+      const end = now();
+      console.log(`Databased seeded in ${end - start} milliseconds`);
+      pool.end();
+      process.exit();
+    });
 };
 
 seedPostgres();
