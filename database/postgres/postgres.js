7e8f7f27e339917fc8ab4aa74ba4c971bbc10b9e
Calvin <calvinminhla@gmail.com>
2021-06-09 19:17:28 -0700

    Added env for database and host. Fix CSS positioning issue due missing image service.

--- database/postgres/postgres.js
+++ database/postgres/postgres.js
@@ -7,8 +7,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  database: 'sdc',
-  port: 5432
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 pool.on('error', (err, client) => {
